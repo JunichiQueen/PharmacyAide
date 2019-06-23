dc1246b72e4dcf58271e939518e15d55f4be65e6
Junichi Queen <junichim@hotmail.com>
2019-06-23 14:13:00 +0100

    populated tables

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,9 @@
 INSERT INTO Condition (id, conditionName) VALUES (1, 'Headache')
 INSERT INTO Condition (id, conditionName) VALUES (2, 'Flu')
+INSERT INTO Condition (id, conditionName) VALUES (3, 'Pertussis')
+INSERT INTO Condition (id, conditionName) VALUES (4, 'Strep Throat')
+INSERT INTO Condition (id, conditionName) VALUES (5, 'Hepatitis C')
+
 
 
 INSERT INTO Medicine (id, drugName, stock) VALUES (1, 'Paracetamol', 130)
@@ -7,6 +11,18 @@ INSERT INTO Medicine (id, drugName, stock) VALUES (2, 'Aspirin', 20)
 INSERT INTO Medicine (id, drugName, stock) VALUES (3, 'Lemsip', 90)
 INSERT INTO Medicine (id, drugName, stock) VALUES (4, 'Lemlift', 10)
 INSERT INTO Medicine (id, drugName, stock) VALUES (5, 'Oseltamivir', 40)
+INSERT INTO Medicine (id, drugName, stock) VALUES (6, 'Erythromycin', 35)
+INSERT INTO Medicine (id, drugName, stock) VALUES (7, 'Penicillin', 115)
+INSERT INTO Medicine (id, drugName, stock) VALUES (8, 'Amoxicillin', 95)
+INSERT INTO Medicine (id, drugName, stock) VALUES (9, 'Ribavirin', 10)
 
 INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (1,1)
 INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (1,2)
+INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (2,3)
+INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (2,4)
+INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (2,5)
+INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (3,6)
+INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (4,7)
+INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (4,8)
+INSERT INTO CONDITION_MEDICINE (CONDITION_ID, MEDICINELIST_ID) VALUES (5,9)
+
